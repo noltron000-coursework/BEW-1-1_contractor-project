5acc7dabeee3498b4a165ac49feeb7a1d2a74bdf
Nolan Kovacik <noltron000@gmail.com>
2018-10-14 23:55:18 -0700

    fixed update route

--- controllers/users.js
+++ controllers/users.js
@@ -18,20 +18,6 @@ function users (app) {
 	});
 
 	// COMPLETE!
-	// SHOW USER
-	app.get('/users/:userId', (req, res) => {
-		User.findById(req.params.userId)
-		.then(user => {
-			res.render('users-show', {
-				user: user
-			});
-		}).catch((err) => {
-			res.status(400)
-			.send({ err: err });
-		});
-	});
-
-	// COMPLETE!
 	// NEW => SHOW USER CREATION FORM
 	app.get('/users/new', (req, res) => {
 		User.findById(req.params.userId)
@@ -47,24 +33,30 @@ function users (app) {
 	});
 
 	// COMPLETE!
-	// CREATE => GENERATE NEW USER FROM FORM
-	app.post('/users', (req, res) => {
-		User.create(req.body)
-		.then((user) => {
-			res.redirect("/")
+	// SHOW USER
+	app.get('/users/:userId', (req, res) => {
+		User.findById(req.params.userId)
+		.then(user => {
+			res.render('users-show', {
+				user: user
+			});
 		}).catch((err) => {
 			res.status(400)
 			.send({ err: err });
 		});
 	});
 
-
 	// UPDATE USER
 	app.put('/users/:userId', (req, res) => {
 		User.findByIdAndUpdate(req.params.userId, req.body)
 		.then(user => {
-			res.redirect(`/users/${user.userId}`)
+			console.log("Update User");
+			console.log(user);
+			console.log("\n");
+			res.redirect(`/users/${user.id}`)
 		}).catch((err) => {
+			console.log("Update User" + err.message)
+			console.log("\n");
 			res.status(400)
 			.send({ err: err });
 		})
@@ -74,26 +66,46 @@ function users (app) {
 	app.get('/users/:userId/edit', (req, res) => {
 		User.findById(req.params.userId, function(err, user) {
 			console.log("Edit User " + user);
+			console.log("\n");
 			res.render('users-edit', { user: user });
 		}).catch((err) => {
 			console.log(err);
+			console.log("\n");
 			res.status(400)
 			.send({ err: err });
 		})
 	})
 
-	// // FIXING...
+	// COMPLETE!
+	// CREATE => GENERATE NEW USER FROM FORM
+	app.post('/users', (req, res) => {
+		User.create(req.body)
+		.then((user) => {
+			res.redirect(`/users/${user.id}`);
+		}).catch((err) => {
+			res.status(400)
+			.send({ err: err });
+		});
+	});
+
+
+	// // COMPLETE!
 	// // DELETE
-	// app.delete('/user/:userId', function (req, res) {
-	// 	User.findByIdAndRemove(req.params.userId)
-	// 	.then(user => {
-	// 		res.status(200)
-	// 		.send({ user: user });
-	// 	}).catch((err) => {
-	// 		res.status(400)
-	// 		.send({ err: err });
-	// 	});
-	// });
+	app.delete('/users/:userId', function (req, res) {
+		User.findByIdAndRemove(req.params.userId)
+		.then(user => {
+			console.log("Delete User Success: ")
+			console.log(user);
+			console.log("\n");
+			res.redirect("/");
+		}).catch((err) => {
+			console.log("Delete User Error: ")
+			console.log(err);
+			console.log("\n");
+			res.status(400)
+			.send({ err: err });
+		});
+	});
 }
 
 
